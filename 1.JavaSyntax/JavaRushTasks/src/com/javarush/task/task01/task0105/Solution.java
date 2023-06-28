1a50118926d71ff1d3c76802f7d9d5a333b50f16
DEBAGanov <baganov.v@gmail.com>
2023-06-29 01:51:00 +0300

    completed task0105

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task01.task0105;
+
+public class Solution {
+    public static void main(String[] args) {
+         // Объявление переменных
+        String name;
+        int age;
+        String city;
+        
+        // Присваивание значений переменным
+        name = "John";
+        age = 25;
+        city = "New York";
+        
+        // Вывод значений переменных
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+    }
+    
+}
