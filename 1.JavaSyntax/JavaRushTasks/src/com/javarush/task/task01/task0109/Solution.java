1011ddf36632d9596284ab1fb0bce906cab2c24f
DEBAGanov <baganov.v@gmail.com>
2023-06-29 02:07:19 +0300

    completed task0109

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task01.task0109;
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 10, b = 15;
+
+        System.out.println("Значение переменной a: " + a);
+        System.out.println("Значение переменной b: " + b);
+    }
+
+}
