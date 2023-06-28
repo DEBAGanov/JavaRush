7f38a3f283bb31d367598d175088d6fed94c5037
DEBAGanov <baganov.v@gmail.com>
2023-06-29 01:46:35 +0300

    completed task0103

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0103/Solition.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0103;
+
+public class Solition {
+    public static void main(String[] args) {
+        int currentYear = 3126;
+        int ageOfFriend = 8;
+
+        int birthYear = currentYear - ageOfFriend;
+
+        System.out.println("Год рождения моего друга: " + birthYear);
+    }
+
+}
