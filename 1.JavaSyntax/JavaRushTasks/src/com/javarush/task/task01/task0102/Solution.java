fea28134903a43d2cbc6a5dd5f8dda163502567f
DEBAGanov <baganov.v@gmail.com>
2023-06-29 00:51:35 +0300

    completed task0102

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0102/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task01.task0102;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Элли красивая");
+        }
+    }
+}
