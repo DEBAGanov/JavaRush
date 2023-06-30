659bb50ece76e0bd75ac1bd2ed94fbd3903dde76
DEBAGanov <baganov.v@gmail.com>
2023-06-30 23:48:36 +0300

    completed task0116

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0116/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task01.task0116;
+
+public class Solution {
+        public static void main(String[] args) {
+        String name = "Ellie";
+        String text = "Hello, " + name + "!";
+        System.out.println(text);
+    }
+}
