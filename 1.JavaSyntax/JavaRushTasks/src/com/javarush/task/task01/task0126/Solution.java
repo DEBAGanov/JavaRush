275d049676479dff60e6125ea9f21f6bd7f537ee
DEBAGanov <baganov.v@gmail.com>
2023-07-02 23:42:03 +0300

    completed task0126

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0126/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task01.task0126;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+            System.out.println("Хочешь - решай задачи, не хочешь - решай нехотя");
+        }
+    }
+}
