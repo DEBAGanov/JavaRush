5d0c1b6ed5ec910181ef5fc7a7486026167cb890
DEBAGanov <baganov.v@gmail.com>
2023-07-03 01:09:58 +0300

    completed task0130

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0130/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task01.task0130;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiusToFahrenheit(41));
+    }
+
+    public static double convertCelsiusToFahrenheit(int celsius){
+        return (9 / 5.0) * celsius + 32;
+    }
+}
