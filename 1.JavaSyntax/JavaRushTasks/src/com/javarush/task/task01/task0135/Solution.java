63fa1d72f8a22ce99e5b8299f7667e9ec682c788
DEBAGanov <baganov.v@gmail.com>
2023-07-04 23:56:31 +0300

    completed task0135

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0135/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0135;
+
+public class Solution {
+    public static void main(String[] args) {
+        String apple = "Яблоко";
+        String porridge = "Каша";
+        String sausage = "Колбаса";
+        String peach = "Персик";
+
+        System.out.println(apple);
+        System.out.println(peach);
+    }
+}
