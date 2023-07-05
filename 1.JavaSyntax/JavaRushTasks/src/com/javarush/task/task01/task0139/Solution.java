3e960acad57211ed4e3479b7a1da557315c8a230
DEBAGanov <baganov.v@gmail.com>
2023-07-06 00:17:10 +0300

    completed task0140

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0139/Solution.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task01.task0139;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Поцелуй мой блестящий металлический зад!");
+    }
+}
