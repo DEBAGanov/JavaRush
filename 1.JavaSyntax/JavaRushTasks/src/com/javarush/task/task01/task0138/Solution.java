252f6607e61c30135f390d1a97880873e9740b49
DEBAGanov <baganov.v@gmail.com>
2023-07-06 00:13:39 +0300

    completed task0138

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task01.task0138;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Мое любимое стихотворение:");
+        }
+    }
+}
