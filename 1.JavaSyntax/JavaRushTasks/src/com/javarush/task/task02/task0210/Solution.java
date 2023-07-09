9c3a44472f2525d24e38594dfd43945adb9e27c0
DEBAGanov <baganov.v@gmail.com>
2023-07-09 12:53:00 +0300

    completed task0210

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task02/task0210/Solution.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task02.task0210;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Будешь похо кодить придет Java и съест товю память.");
+    }
+}
