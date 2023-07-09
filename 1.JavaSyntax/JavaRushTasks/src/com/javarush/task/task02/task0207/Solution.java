24b84aab2b15c63983c17bf202e9bc6257f68a74
DEBAGanov <baganov.v@gmail.com>
2023-07-09 12:33:50 +0300

    completed task0207

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task02/task0207/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task02.task0207;
+
+public class Solution {
+    public static void main(String[] args) {
+        int b = 7, a = 5;
+        System.out.println(a + b);
+        System.out.println(a * b);
+    }
+}
