3c9ec00f297011f8b33ecfaeefb8a84fd2f0cde8
DEBAGanov <baganov.v@gmail.com>
2023-07-09 12:23:19 +0300

    completed task0205

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task02/task0205/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task02.task0205;
+public class Solution {
+    public static void main(String[] args) {
+        hackSalary(700);
+    }
+
+    public static void hackSalary(int b){
+        int a = 100;
+        System.out.println("Твоя зарплата состовляет: "+ (a + b) + " долларов в месяц.");
+    }
+}
