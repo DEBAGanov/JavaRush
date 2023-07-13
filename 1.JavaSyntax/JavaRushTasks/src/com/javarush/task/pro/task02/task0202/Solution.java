6d1114f2691af003d1088ed909c548ddfe167ae1
DEBAGanov <baganov.v@gmail.com>
2023-07-14 02:14:34 +0300

    completed pro_task0202

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0202;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    int centimeters = 300;
+    double meters = centimeters / 100;
+
+    System.out.println(meters);
+  }
+}
