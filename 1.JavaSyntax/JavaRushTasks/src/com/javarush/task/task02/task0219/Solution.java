fddd9af6024acd8e1bf5b027c55320f36f996e7a
DEBAGanov <baganov.v@gmail.com>
2023-07-13 23:18:38 +0300

    completed task0219

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task02/task0219/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task02.task0219;
+
+public class Solution {
+    public static void print3(String s) {
+        System.out.print(s +" ");
+        System.out.print(" "+ s +" ");
+        System.out.print(" "+ s + " ");
+
+    }
+
+    public static void main(String[] args) {
+        print3("window");
+        print3("file");
+    }
+}
