7ef0017534a8fd76b04a134696a1f9e116cc8d47
DEBAGanov <baganov.v@gmail.com>
2023-07-17 23:16:40 +0300

    completed pro_task0219

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0219;
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 1;
+        int age = 35;
+        String documents = "Passport";
+        String behavior = "Good";
+
+    }
+}
