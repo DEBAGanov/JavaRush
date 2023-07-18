4f078e4075331e3f3a4a0e8ca8df2fa290021dea
DEBAGanov <baganov.v@gmail.com>
2023-07-18 23:33:15 +0300

    completed pro_task0305

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task03.task0305;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numb1 = scanner.nextInt();
+        int numb2 = scanner.nextInt();
+        int numb3 = scanner.nextInt();
+
+        if (numb1 == numb2 && numb2 == numb3) {
+            System.out.println(numb1 + " " + numb2 + " " + numb3);
+        } else if (numb1 == numb2) {
+            System.out.println(numb1 + " " + numb2);
+        } else if (numb2 == numb3) {
+            System.out.println(numb2 + " " + numb3);
+        } else if (numb1 == numb3) {
+            System.out.println(numb1 + " " + numb3);
+        } else {
+            System.out.println("Пар не найдено");
+        }
+    }
+}
