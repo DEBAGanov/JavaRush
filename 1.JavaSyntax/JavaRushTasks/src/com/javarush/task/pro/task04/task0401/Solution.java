471ef453e19e817c4fc637099d6887e3e3b8341a
DEBAGanov <baganov.v@gmail.com>
2023-07-23 17:25:21 +0300

    completed pro_task0401

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task04.task0401;
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+            i++;
+        }
+    }
+}
