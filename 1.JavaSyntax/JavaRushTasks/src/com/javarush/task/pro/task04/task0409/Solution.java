bff62ab37860393f0edab860b23947fa5f52cd10
DEBAGanov <baganov.v@gmail.com>
2023-07-25 23:44:17 +0300

    completed pro_task0409

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task04/task0409/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0409;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min = Integer.MAX_VALUE;
+        while (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            if (number < min) {
+                min = number;
+            }
+        }
+        System.out.println(min);
+    }
+}
