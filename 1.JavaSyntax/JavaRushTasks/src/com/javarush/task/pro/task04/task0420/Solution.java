3d61c3756f5a0e2ddd3dda4173aec4f4ee6a34b8
DEBAGanov <baganov.v@gmail.com>
2023-07-30 00:51:39 +0300

    completed pro_task0420

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task04/task0420/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task04.task0420;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Введите предполагаемое число");
+
+        checkNumber(5);
+    }
+
+    public static void checkNumber(int number) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber;
+
+        while (true) {
+            inputNumber = scanner.nextInt();
+            if (number == inputNumber) {
+                System.out.println("Загадочное число равно: " + number);
+                break;
+            }
+            System.out.println("Попробуй еще раз");
+        }
+    }
+}
