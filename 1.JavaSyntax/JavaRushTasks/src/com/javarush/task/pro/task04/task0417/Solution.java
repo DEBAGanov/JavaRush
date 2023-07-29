6b4beb5f587f66cbf60de8c5ba122a7442b4ff02
DEBAGanov <baganov.v@gmail.com>
2023-07-29 23:58:56 +0300

    completed pro_task0417

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task04/task0417/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task04.task0417;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        System.out.println(Math.round(number * 3.6));
+    }
+}
