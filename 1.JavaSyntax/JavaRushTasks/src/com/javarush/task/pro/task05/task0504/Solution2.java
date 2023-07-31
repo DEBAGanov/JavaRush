1f03db552fad33b3b62536901ec777fad116fb88
DEBAGanov <baganov.v@gmail.com>
2023-08-01 01:19:46 +0300

    completed pro_task0504

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task05/task0504/Solution2.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task05.task0504;
+
+import java.util.Arrays;
+
+public class Solution2 {
+    public static int[] firstArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    public static int[] secondArray = new int[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+
+        resultArray = new int[firstArray.length + secondArray.length];
+
+        resultArray = Arrays.copyOf(firstArray, firstArray.length + secondArray.length);
+
+        System.arraycopy(secondArray, 0, resultArray, firstArray.length, secondArray.length);
+
+        for (int number : resultArray) {
+            System.out.print(number + ", ");
+        }
+    }
+}
