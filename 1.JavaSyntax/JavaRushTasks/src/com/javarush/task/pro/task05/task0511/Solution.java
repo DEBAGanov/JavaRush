366708e69e945b0dcdcdc76cdc47dd005499b8d9
DEBAGanov <baganov.v@gmail.com>
2023-08-05 11:42:37 +0300

    completed pro_task0511

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task05.task0511;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введитите кол-во строк");
+        int line = scanner.nextInt();
+        System.out.println("Введитите кол-во столбцов");
+        int columns = scanner.nextInt();
+
+        int multiArray [][] = new int[line][];
+        System.out.println(multiArray);
+    }
+}
