0d826f0bfcf4c74715fac718432265d15add883f
DEBAGanov <baganov.v@gmail.com>
2023-08-12 23:43:00 +0300

    completed pro_task0525

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task05/task0525/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task05.task0525;
+
+public class Solution {
+    public static String[] array;
+
+    public static void main(String[] args) {
+        array = new String[] { "robotank", "robotank", "robotank", "robotank", "robotank", "robotank" };
+
+        boolean isMissionComplete = false;
+        for (String string : array) {
+            if ("robotank".equalsIgnoreCase(string)) {
+                isMissionComplete = true;
+                break;
+            }
+        }
+        System.out.println(isMissionComplete ? "Mission complete" : "Mission not completed");
+    }
+}
