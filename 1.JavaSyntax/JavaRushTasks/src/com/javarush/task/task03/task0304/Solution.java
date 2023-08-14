ca4e338ddf0418811b7c6aa3bae3158ad8195c43
DEBAGanov <baganov.v@gmail.com>
2023-08-15 00:27:22 +0300

    completed task0304

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task03/task0304/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0304;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(addTenPercent(100));
+
+    }
+
+    public static double addTenPercent(int number) {
+        return number * 1.1;
+    }
+}
