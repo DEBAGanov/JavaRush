fd5f980ef7e6edcbf89f27de1826c3ea7c5282ea
DEBAGanov <baganov.v@gmail.com>
2023-08-16 01:36:35 +0300

    completed task0308

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task03/task0308/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task03.task0308;
+
+public class Solution {
+    public static void main(String[] args) {
+        int sum = 1;
+        for (int i = 1; i < 11; i++) {
+            sum = sum * i;
+        }
+        System.out.println(sum);
+
+        System.out.println(2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+    }
+}
