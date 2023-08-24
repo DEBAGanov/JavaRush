7137a10eb02289a328510a4aabf6d5521c6f3d0c
DEBAGanov <baganov.v@gmail.com>
2023-08-25 00:17:17 +0300

    completed task0404

--- 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task04/task0404/Cat.java
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task04/task0404/Cat.java
@@ -1,4 +1,5 @@
 package com.javarush.task.task04.task0404;
+
 public class Cat {
     private static int catsCount = 0;
 
@@ -7,8 +8,6 @@ public class Cat {
     }
 
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.addNewCat();
-        System.out.println(Cat.catsCount);
+
     }
 }
