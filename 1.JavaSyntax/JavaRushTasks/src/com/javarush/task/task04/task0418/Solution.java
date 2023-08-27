c0a0c1f0222cd152d04133d1a30eb67ef212f88e
DEBAGanov <baganov.v@gmail.com>
2023-08-27 23:32:24 +0300

    completed task0413

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task04/task0418/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task04.task0418;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        System.out.println(Math.min(a, b));
+
+    }
+}
