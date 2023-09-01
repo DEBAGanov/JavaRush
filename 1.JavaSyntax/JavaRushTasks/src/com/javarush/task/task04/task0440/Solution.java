52a2458fe6e63425dd52a5c3d0c450fb4284ba0b
DEBAGanov <baganov.v@gmail.com>
2023-09-01 23:59:19 +0300

    completed task0440

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task04/task0440/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task04.task0440;
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            System.out.println("Я никогда не буду работать за копейки. Амиго");
+            i++;
+        }
+    }
+}
