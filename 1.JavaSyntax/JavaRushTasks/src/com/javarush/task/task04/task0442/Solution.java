dfbaac03484044d935b6279073b50218f76e10e1
DEBAGanov <baganov.v@gmail.com>
2023-09-02 10:44:26 +0300

    completed task0442

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task04/task0442/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task04.task0442;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = 0, b = 0;
+        while (a != -1) {
+            a = Integer.parseInt(r.readLine());
+            b = b + a;
+        }
+        System.out.println(b);
+    }
+}
