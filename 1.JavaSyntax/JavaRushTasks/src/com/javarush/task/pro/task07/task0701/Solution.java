5a4efd9b83fc1892f4689e4c57323333d488b339
DEBAGanov <baganov.v@gmail.com>
2023-09-03 23:54:47 +0300

    completed pro_task0701

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0701;
+
+public class Solution {
+    boolean aBoolean;
+    byte aByte;
+    short aShort;
+    int anInt;
+    char aChar;
+    long aLong;
+
+    float aFloat;
+    double aDouble;
+}
