2f0691a214694070e5ff667d449d811d9a9d8dee
DEBAGanov <baganov.v@gmail.com>
2023-09-03 23:58:46 +0300

    completed pro_task0705

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task07/task0705/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0705;
+
+public class Solution {
+    public static void main(String[] args) {
+        div(0, 15);
+        div(0, -15);
+    }
+
+    public static void div(double a, double b) {
+        System.out.println(b / a);
+    }
+}
