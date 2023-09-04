4173520178ae45b5ee107a8b9a0d64d3a27e0cad
DEBAGanov <baganov.v@gmail.com>
2023-09-04 23:31:31 +0300

    completed pro_task0712

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task07.task0712;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+
+        int[] a = new int[5];
+        int[] b = new int[5];
+        int[] c = new int[5];
+        int[] d = new int[5];
+        int[] f = new int[5];
+
+        new Scanner(System.in);
+        new Scanner(System.in);
+
+    }
+}
