06467e3f2a3db70e3a8eb2e6f5e480cc8938199d
DEBAGanov <baganov.v@gmail.com>
2023-09-05 12:17:23 +0300

    completed pro_task0719

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task07/task0719/Sollution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0719;
+
+public class Sollution {
+    public static void main(String[] args) {
+        AutomaticKitchen automaticKitchen = new AutomaticKitchen();
+        Gun gun = new Gun();
+        ProtectiveFieldGenerator protectiveFieldGenerator = new ProtectiveFieldGenerator();
+        RepairDrone repairDrone = new RepairDrone();
+        RobotCleaner robotCleaner = new RobotCleaner();
+
+    }
+}
