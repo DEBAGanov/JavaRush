13fb8d829b463c18eabf7aa47841dc5b4629aae4
DEBAGanov <baganov.v@gmail.com>
2023-09-07 23:48:13 +0300

    completed pro_task0805

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task08/task0805/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task08.task0805;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Заработаю ли я себе на BMW?");
+        System.out.println(MagicBall.getPrediction());
+    }
+}
