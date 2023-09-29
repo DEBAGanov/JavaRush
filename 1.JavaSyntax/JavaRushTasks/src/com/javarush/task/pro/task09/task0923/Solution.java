7f3ec92b65c5e542a7d12adf442e35ef9092a0b7
DEBAGanov <baganov.v@gmail.com>
2023-09-29 22:27:54 +0300

    completed pro_task0923

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/pro/task09/task0923/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task09.task0923;
+
+import static com.javarush.task.pro.task09.task0923.CaptainBridge.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] ranks = {"Раб", "Гость", "Боец", "Ветеран", "Картографист", "Капитан", "Стив"};
+
+        String str = "";
+        for (String rank : ranks) {
+            if(debugAccess(rank)){
+                str = rank;
+            }
+        }
+        checkAccess(str);
+    }
+}
