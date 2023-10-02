a4512ffeb4b49fb0cb86095706d453ea1fd1f232
DEBAGanov <baganov.v@gmail.com>
2023-10-02 22:21:18 +0300

    completed task0511

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task05/task0511/Dog.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task05.task0511;
+
+public class Dog {
+    String name;
+    int height;
+    String color;
+
+    public void initialize(String nane) {
+        this.name = name;
+    }
+
+    public void initialize(String nane, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void initialize(String nane, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
