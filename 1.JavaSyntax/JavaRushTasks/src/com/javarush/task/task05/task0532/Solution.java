051ddb42138892c476fc71334855cc33f1fa2e5a
DEBAGanov <baganov.v@gmail.com>
2023-10-10 17:43:08 +0300

    completed task0532

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task05/task0532/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task05.task0532;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(reader.readLine());
+            int maximum = Integer.MIN_VALUE;
+            if (N > 0) {
+                for (int i = 0; i < N; i++) {
+                    int chislo = Integer.parseInt(reader.readLine());
+                    if (chislo > maximum)
+                        maximum = chislo;
+                }
+            }
+            System.out.println(maximum);
+        }
+    }
+}
