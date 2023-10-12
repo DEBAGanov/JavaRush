1dfe36dd1a0cf75fe08db48acdf70cd4f041683f
DEBAGanov <baganov.v@gmail.com>
2023-10-12 23:54:07 +0300

    completed task0601

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task06/task0601/Cat.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task06.task0601;
+
+public class Cat {
+
+    public static void main(String[] args) {
+
+    }
+
+    protected void finalize() throws Throwable {
+
+    }
+
+}
