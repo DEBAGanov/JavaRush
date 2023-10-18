3210ce08a3286703d3637fd5a481e25dd12486de
DEBAGanov <baganov.v@gmail.com>
2023-10-18 23:56:39 +0300

    completed task0607

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task06/task0607/Cat.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task06.task0607;
+
+public class Cat {
+    static int catCount;
+
+    public Cat() {
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
