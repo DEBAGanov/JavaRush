f9d6802c9504c43cec88610558cbe7837c312b10
DEBAGanov <baganov.v@gmail.com>
2023-10-23 23:53:58 +0300

    completed task0613

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task06/task0613/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task06.task0613;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            new Cat();
+        }
+        System.out.print(Cat.catCount);
+    }
+
+    public static class Cat {
+
+        static public int catCount;
+
+        public Cat() {
+            catCount++;
+        }
+    }
+}
