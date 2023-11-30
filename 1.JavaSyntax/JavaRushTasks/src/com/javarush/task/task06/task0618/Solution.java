c95cbf04b0b8c656935c3d018d68e1a4ae933f9e
DEBAGanov <baganov.v@gmail.com>
2023-12-01 00:00:24 +0300

    completed task0618

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task06/task0618/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task06.task0618;
+
+public class Solution {
+    public static class KissMyShinyMetalAss {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new KissMyShinyMetalAss());
+    }
+}
