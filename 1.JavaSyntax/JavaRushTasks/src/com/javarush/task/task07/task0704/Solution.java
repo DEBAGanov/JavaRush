358304647a46ba05acb1af2c1666204fe889ef11
DEBAGanov <baganov.v@gmail.com>
2023-12-30 00:24:47 +0300

    completed task0704

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task07/task0704/Solution.java
@@ -0,0 +1,19 @@
+
+package com.javarush.task.task07.task0704;
+
+import java.util.Scanner;
+
+public abstract class Solution {
+    public static void main(String[] args) {
+        int arr[] = new int[3];
+        System.out.println("Введите 10 чисел: ");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Список в обратном порядке: ");
+        for (int i = arr.length - 1; i < arr.length; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
