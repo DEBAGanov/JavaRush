3f60069c1521c919d986eef6698681bf0fb9fd85
Vladimir Baganov <baganov.v@gmail.com>
2025-02-18 20:35:06 +0300

    completed task0801

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0801/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task08.task0801;
+
+import java.util.HashSet;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        HashSet<String> plants = new HashSet<>();
+        plants.add("арбуз");
+        plants.add("банан");
+        plants.add("вишня");
+        plants.add("груша");
+        plants.add("дыня");
+        plants.add("ежевика");
+        plants.add("жень-шень");
+        plants.add("земляника");
+        plants.add("ирис");
+        plants.add("картофель");
+        plants.add("картофель");
+
+        for (String plant : plants) {
+            System.out.println(plant);
+        }
+    }
+}
