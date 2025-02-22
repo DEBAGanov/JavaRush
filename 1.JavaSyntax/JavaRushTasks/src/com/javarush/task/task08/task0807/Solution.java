984e141f0ac5bf21fb8ee200cf8208d3908e119a
Vladimir Baganov <baganov.v@gmail.com>
2025-02-22 19:21:27 +0300

    completed task087

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Solution {
+    public static void main(String[] args) {
+        CreateArrayList();
+        CreateLinkedList();
+    }
+
+    public static ArrayList<String> CreateArrayList() {
+        return new ArrayList<>();
+    }
+
+    public static LinkedList<String> CreateLinkedList() {
+        return new LinkedList<>();
+    }
+
+}
