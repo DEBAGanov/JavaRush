6d2acab59916d15a3fcc89b6dcfb60b6d34537f4
Vladimir Baganov <baganov.v@gmail.com>
2025-02-23 18:02:41 +0300

    completed task0811

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0811/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task08.task0811;
+
+public class Solution {
+    public static vois main(String[] args) {
+
+    }
+
+    public static List getListForGet() {
+        return new ArrayList<>();
+    }
+
+    public static List getListForSet() {
+        return new ArrayList<>();
+    }
+
+    public static List getListForAddOrInsert() {
+        return new LinkedList<>();
+    }
+
+    public static List getListForRemove() {
+        return new LinkedList<>();
+    }
+}
