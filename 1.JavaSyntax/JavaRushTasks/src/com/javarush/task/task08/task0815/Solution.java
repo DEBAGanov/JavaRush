cc711b1631a32d40ce3d4973d1c416a0d51a5ffb
Vladimir Baganov <baganov.v@gmail.com>
2025-02-26 23:17:37 +0300

    completed task0815

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0815/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task08.task0815;
+
+public class Solution {
+    public static main (String [] args ) {
+        HashMap<String, String> map = createMap();
+
+    }
+
+    public static Map<String, String> createMap() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Иванов", "Иван");
+        map.put("Петров", "Петр");
+        map.put("Сидоров", "Сидор");
+        map.put("Кузнецов", "Кузьма");
+        map.put("Смирнов", "Сергей");
+        map.put("Попов", "Павел");
+        map.put("Соколов", "Сергей");
+        map.put("Михайлов", "Михаил");
+        map.put("Федоров", "Федор");
+        map.put("Морозов", "Максим");
+        map.put("Иванов", "Иван");
+
+    }
+
+    public static int getCountTheSameFirsName(Map<String, String> map) {
+
+    }
+
+    public static int getCountTheSameLastName(Map<String, String> map) {
+
+    }
+
+}
