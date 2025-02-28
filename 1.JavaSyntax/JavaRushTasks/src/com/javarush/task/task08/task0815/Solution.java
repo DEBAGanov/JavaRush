abe459cd6fd80292aa8c0ec043a59cf3ecff554c
Vladimir Baganov <baganov.v@gmail.com>
2025-02-28 23:41:40 +0300

    completed task0815

--- 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0815/Solution.java
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0815/Solution.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task08.task0815;
 
-public class Solution {
-    public static main (String [] args ) {
-        HashMap<String, String> map = createMap();
+import java.util.HashMap;
+import java.util.Map;
 
+public class Solution {
+    public static void main(String[] args) {
+        HashMap<String, String> map = createMap();
+        System.out.println(getCountTheSameFirstName(map));
+        System.out.println(getCountTheSameLastName(map, "Иванов"));
     }
 
     public static Map<String, String> createMap() {
@@ -18,16 +22,26 @@ public class Solution {
         map.put("Михайлов", "Михаил");
         map.put("Федоров", "Федор");
         map.put("Морозов", "Максим");
-        map.put("Иванов", "Иван");
-
+        return map;
     }
 
-    public static int getCountTheSameFirsName(Map<String, String> map) {
-
+    public static int getCountTheSameFirstName(Map<String, String> map, String name) {
+        int count = 0;
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            if (pair.getValue().equals(name)) {
+                count++;
+            }
+        }
+        return count;
     }
 
-    public static int getCountTheSameLastName(Map<String, String> map) {
-
+    public static int getCountTheSameLastName(Map<String, String> map, String lastName) {
+        int count = 0;
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            if (pair.getKey().equals(lastName)) {
+                count++;
+            }
+        }
+        return count;
     }
-
 }
