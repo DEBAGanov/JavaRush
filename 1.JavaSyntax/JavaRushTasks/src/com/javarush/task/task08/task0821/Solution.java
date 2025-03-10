af86d623a2250daded315ec5713e75dd9e5aebcc
Vladimir Baganov <baganov.v@gmail.com>
2025-03-10 23:59:32 +0300

    completed task0821

---
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0821/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task08.task0821;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleList();
+        printPeopleList(map);
+    }
+
+}
