3d635d4452b8d7526f0aaa60a4f7e142d78c434f
Vladimir Baganov <baganov.v@gmail.com>
2025-03-19 23:56:58 +0300

    completed task0821

--- 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0821/Solution.java
+++ 1.JavaSyntax/JavaRushTasks/src/com/javarush/task/task08/task0821/Solution.java
@@ -9,4 +9,27 @@ public class Solution {
         printPeopleList(map);
     }
 
+    public static Map<String, String> createPeopleList() {
+        Map<String, String> map = new HashMap<>();
+
+        // Добавляем людей с одинаковыми фамилиями
+        map.put("Иванов", "Петр");
+        map.put("Иванов", "Александр");
+        map.put("Петров", "Иван");
+        map.put("Сидоров", "Иван");
+        map.put("Смирнов", "Алексей");
+        map.put("Кузнецов", "Дмитрий");
+        map.put("Смирнов", "Сергей");
+        map.put("Попов", "Александр");
+        map.put("Соколов", "Дмитрий");
+        map.put("Морозов", "Андрей");
+
+        return map;
+    }
+
+    public static void printPeopleList(Map<String, String> map) {
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
 }
